d158b07d4bd5db8644438b3bac1d5acf73062152
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-10-20 16:34:24 +0300

    done3

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
 const body = document.querySelector("body");
+body.className = 'navbar-header';
+body.style.margin = "auto";
 const h1 = document.createElement("h1");
 h1.innerHTML = "Todo List";
+h1.style.flexDirection = "center"
 body.append(h1);
 
 const listI = document.createElement("ul");
 listI.id = "ulID";
+listI.style.margin = "auto";
+listI.className ="dropdown-menu"
+listI.className = 'collapsed'
 body.append(listI);
 
 const toDos = ["wake up", "eat breakfast", "code"];
 
-
 const delItem = (e) => {
-    
-    console.log(e.target);
+  // console.log(e.target);
 
-   
-    toDos.splice(e.target.id,1);
-    renderList();
-    
-    
-    }
-     const input2 = document.createElement("input")
-      body.append(input2)
-      input2.placeholder ="write your update here"
-      input2.style.display = 'block';
-
+  toDos.splice(e.target.id, 1);
+  renderList();
+};
+const input2 = document.createElement("input");
+body.append(input2);
+input2.placeholder = "write your update here";
+input2.style.display = "block";
 
 const updItem = (e) => {
-   if (input2.value === "") { 
-       return
-   }
-toDos.splice(e.target.id , 1 ,input2.value )
+  if (input2.value === "") {
+    return;
+  }
+  toDos.splice(e.target.id, 1, input2.value);
 
-renderList();
-}
-
-
-
+  renderList();
+};
 
 const renderList = () => {
   const ulList = document.querySelector("#ulID");
@@ -46,12 +43,16 @@ const renderList = () => {
     newLI.innerHTML = toDos[index];
     const btn2 = document.createElement("button");
     btn2.addEventListener("click", delItem);
-    btn2.id = index
-    btn2.innerHTML ="Delete"
+    btn2.id = index;
+    btn2.className ="btn"
+    btn2.className = 'btn-danger'
+    btn2.innerHTML = "Delete";
     const btn3 = document.createElement("button");
     btn3.addEventListener("click", updItem);
     btn3.id = index;
-    btn3.innerHTML ="UpDate"
+    btn3.className = 'btn'
+    btn3.className = 'btn-success'
+    btn3.innerHTML = "UpDate";
     ulList.append(newLI);
     ulList.append(btn2);
     ulList.append(btn3);
@@ -64,23 +65,19 @@ const addToList = () => {
   renderList();
 };
 
-
-
 const btn1 = document.createElement("button");
-btn1.id ="btn1"
+btn1.id = "btn1";
+btn1.className ="btn"
+btn1.className = 'btn-primary'
+
 btn1.innerHTML = "Add";
 body.append(btn1);
+
+
 btn1.addEventListener("click", addToList);
 const input1 = document.createElement("input");
 input1.id = "box";
 input1.placeholder = "Add here";
 body.append(input1);
-
-//const btn2 = document.querySelector("#btn2").style.color = "#ff0000";
-
-
-
-
-
 
 
